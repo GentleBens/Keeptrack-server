a652ec587964b2d4e9ef92506f0082f741eec2dc
stacyburris <stacy1burris@gmail.com>
2021-05-16 12:13:18 -0700

    changed schema

--- src/models/counter.js
+++ src/models/counter.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const counterSchema = new mongoose.Schema({
     counter: {type: Number, required: true},
+    graph: {type: Number, required: false},
+    
   })
 
 module.exports = mongoose.model('counter', counterSchema); 
