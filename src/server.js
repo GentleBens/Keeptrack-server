037a7609bda99ba5087d5e048eb0f3a4270c93b7
JP <jpjones22@gmail.com>
2021-05-31 16:12:14 -0700

    Find all documents. Add documents

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ const notFound = require('./error-handlers/404');
 const serverError = require('./error-handlers/500');
 const apiRoutes = require('./routes/apiRoutes.js');
 const logger = require('./middleware/logger');
+const { Console } = require('console');
 
 //app middleware
 app.use(cors());
@@ -85,11 +86,6 @@ async function printData(desiredDate) {
   console.log('no find it');
   return false;
 }
-console.log(printData(21).toString());
-
-
-
-
 // add to socket.io server
 //let counter = 0;
 // io.on('connection', (socket) => {
@@ -144,33 +140,64 @@ module.exports = {
 //     console.log('a user connected');
 //   });
 
-async function getAll() {
-  return Counter.find()
-    .then((data) => data)
-    .catch(e => { console.log(e) });
-}
 
-
-async function printData(desiredDate) {
-  let dbase = await getAll();
-  //console.log(dbase[0]);
-  for (let i = 0; i < dbase.length; i++) {
-    let dbaseDate = dbase[i].dailyTotal.date.getDate();
-    if (desiredDate === dbaseDate) {
-      console.log('found it');
-      return true;
-    }
-  }
-  console.log('no find it');
-  return false;
-}
 function callBackHandler(req, res, next) {
   res.status(200).send('Hello World');
 }
+//JPJ - This sill get all the documents in the database
+async function getAll() {
+  return Counter.find()
+    .then((data) => data)
+    .catch(e => { console.log(`Error Found: ${e}`) });
+}
+//Add a document entry for a desired date and countnumber TODO: add an update to this document
+async function addDailyTotalData(desDate, count) {
+  let entryDate = new Date();
+  entryDate = desDate;
+  Counter.create({
+    counter: 0,
+    dailyTotal: {
+      date: entryDate,
+      numberCount: count
+    }
+  }, function (err) {
+    if (err) return handleError(err);
+    console.log('Entry Saved');
+  });
+}
+// Prints out all the documents in the database as a console.log
+async function printAllDbData() {
+  let dbase = await getAll();
+  dbase.forEach(i => {
+    let formattedDate = `${i.dailyTotal.date.getMonth()}/${i.dailyTotal.date.getDate()}/${i.dailyTotal.date.getFullYear()}`;
+    console.log(`Date: ${formattedDate} Count: ${i.dailyTotal.numberCount}`);
+    console.log(`Full Format ${i.dailyTotal.date}}`);
+  });
+}
 
-var boolStuff = '123';
-console.log(`umm ${boolStuff}`);
-boolStuff = printData(27);
-console.log(`umm ${boolStuff}`);
+//TODO: get it to actually search.  Perhaps change the schema to date and count.
+//
+//  const counterSchema = new mongoose.Schema({      
+//  date: {type: Date, require: false},
+//  numberCount: {type: Number, require: false},  
+//});
+// Would make it easier to scan non nested documents.
 
-console.log(typeof (boolStuff));
+async function findDbDocument(searchDate) {
+  let countNum = 200;
+  console.log(`Date to search: ${searchDate}`);
+  console.log(`CountNumber: ${countNum}`);
+  let data = await Counter.findOne({ dailyTotal: { numberCount = 200 } }).exec();
+  console.log(`Data: ${data}`);
+}
+//TODO: Create a function to find and update or create the document for the totals for the day if the save totals is pressed
+//this will reset the counter held in memory so as not to distort the total daily count.
+
+//Execution lines
+
+printAllDbData();
+let searchDate = new Date('5/31/2021');
+
+findDbDocument(searchDate);
+
+
